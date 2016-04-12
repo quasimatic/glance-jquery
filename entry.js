594442de329c3c612705ab1ed90d5e4d3acb3108
Dan Gilkerson <contact@dangilkerson.com>
2016-04-11 21:24:38 -0700

    Create function wrapping elements in jQuery

---
+++ entry.js
@@ -0,0 +1,17 @@
+var $ = require('jquery');
+
+var glanceSelector = require('glance-selector').default;
+
+window.glanceSelector = glanceSelector;
+
+window.$glance = function (selector) {
+    return $(glanceSelector(selector));
+};
+
+window.$glance.addCustomLabels = function (labels) {
+    glanceSelector.addCustomLabels(labels);
+};
+
+window.$glance.setLogLevel = function (level) {
+    glanceSelector.setLogLevel(level);
+}
